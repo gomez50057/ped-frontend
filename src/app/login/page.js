c7fe84a25d8f6f876bc7ab4ece88f8edcff2e153
gomez50057 <gomez.50057@gmail.com>
2025-06-10 17:05:13 -0600

    login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,8 +3,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import styles from './login.module.css';
-const imgBasePath = "/img/login/";
-
+const videoBasePath = '/video/';
 
 export default function LoginPage() {
   const router = useRouter();
@@ -15,7 +14,6 @@ export default function LoginPage() {
   const submit = async (e) => {
     e.preventDefault();
     try {
-      console.log('Haciendo POST a:', '/api/login/');
       await axios.post('/api/login/', { username: user, password: pass }, { withCredentials: true });
       router.replace('/dashboard');
     } catch (e) {
@@ -24,53 +22,60 @@ export default function LoginPage() {
   };
 
   return (
-    <section>
-      <form onSubmit={submit} className={styles.login}>
+    <section className={styles.login}>
+      {/* Fondo de video */}
+      <video
+        className={styles.bgVideo}
+        src={`${videoBasePath}bgLogin.mp4`}
+        autoPlay
+        muted
+        loop
+      ></video>
 
-        <div className={styles.containerInicioSeccion}>
-          <div className={styles.imgInicio}>
-            <img src={`${imgBasePath}inicio de seccion.png`} alt="inicio de seccion" />
+      {/* Capa de degradado */}
+      <div className={styles.overlay}></div>
+
+      {/* Contenido principal */}
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <h1><span className='spanDoarado'>Hidalgo,</span> <span>Potencia</span> en <span>Marcha</span> </h1>
+          <h2>El Futuro Comienza Contigo</h2>
+          <p>Inicia sesión con tu usuario y contraseña para participar y ser parte del cambio. Construyamos juntos el futuro de Hidalgo.</p>
+        </div>
+        <form onSubmit={submit} className={styles.form}>
+          <div className={styles.inputGroup}>
+            <input
+              id="username"
+              className={styles.input}
+              type="text"
+              placeholder=" "
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              required
+            />
+            <label htmlFor="username">Usuario</label>
           </div>
           <div className={styles.inputGroup}>
-          <input
-            id="username"
-            className={styles.input}
-            type="text"
-            placeholder=" "        /* <- un espacio para habilitar :placeholder-shown */
-            value={user}
-            onChange={e => setUser(e.target.value)}
-            required
-          />
-          <label htmlFor="username">Usuario</label>
-        </div>
-
-        {/* Grupo Contraseña */}
-        <div className={styles.inputGroup}>
-          <input
-            id="password"
-            className={styles.input}
-            type="password"
-            placeholder=" "
-            value={pass}
-            onChange={e => setPass(e.target.value)}
-            required
-          />
-          <label htmlFor="password">Contraseña</label>
-        </div>
-
-          {err && <p className={styles.error}>{err}</p>}
-          <button type="submit" className={styles.button}>
-            Entrar
-          </button>
-
-        </div>
-
-        <div className={styles.containerLoginImg}>
-          <div className={styles.imgLogin}>
-            <img src={`${imgBasePath}imgLogin.png`} alt="Login" />
+            <input
+              id="password"
+              className={styles.input}
+              type="password"
+              placeholder=" "
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Contraseña</label>
           </div>
-        </div>
-      </form>
+          {err && <p className={styles.error}>{err}</p>}
+          <button type="submit" className={styles.learnMore}>
+            <span className={styles.circle} aria-hidden="true">
+              <span className={`${styles.icon} ${styles.arrow}`}></span>
+            </span>
+            <span className={styles.buttonText}>Entrar</span>
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
