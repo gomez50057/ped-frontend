434303161ab8d6dd2466576c829cb56185dd53cf
gomez50057 <gomez.50057@gmail.com>
2025-07-14 11:24:19 -0600

    Se agrega GoogleAnalytics

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,11 +2,13 @@
 
 import { PlatformProvider } from "@/context/PlatformContext";
 import ClientLayout from "@/components/shared/ClientLayout";
+import GoogleAnalytics from "@/components/shared/GoogleAnalytics";
 
 export default function DashboardLayout({ children }) {
   return (
     <PlatformProvider>
       <ClientLayout>
+        <GoogleAnalytics />
         {children}
       </ClientLayout>
     </PlatformProvider>
